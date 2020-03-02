5f5d40778d81b1672e619770aef486ca414b73d8
ireshmm <socialexpz1@gmail.com>
2020-03-02 09:28:16 +0530

    separate file for initializing environment

---
+++ ace/initenv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+initEnv() {    
+    # Setup environment for ACE toolkit    
+    . /opt/IBM/ace-11.0.0.7/server/bin/mqsiprofile    
+    
+    #Make sure the X server or Xvfb is running    
+    if ! ( pgrep -x Xorg > /dev/null || pgrep -x Xvfb > /dev/null ); then    
+       # Start Xvfb    
+        Xvfb :99 &    
+    fi    
+    
+    if ! [ -n "$DISPLAY" ]; then    
+        # Export display environment variable    
+        export DISPLAY=:99    
+    fi    
+}     
+
+initEnv
