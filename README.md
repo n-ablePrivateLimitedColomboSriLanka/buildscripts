76984793ea26160d7ac1af60c57c8cac09dafe94
Iresh Madhusanka <socialexpz1@gmail.com>
2020-10-27 14:01:26 +0530

    add was deploy script and example invocation to the readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # iresh-s-buildscripts
-changed
-changed
-changed
-changed
-changed
+
+## WAS deployscript invocation example:
+##### Please note that the user.dir should be where the .ear files exist
+
+```bash
+/SOA/bpmdev/IBM/BPM/profiles/BPMDEVDmgr/bin/wsadmin.sh -lang jython -javaoption "-Djython.package.path=/SOA/bpmdev/IBM/BPM/plugins/com.ibm.ws.wlm.jar" -javaoption "-Duser.dir=/SOA/bpmdev/DevEnviromentEAR" -f /SOA/bpmdev/wsadmin-scripts/deploy.py
+```
---
+++ was/deploy.py
@@ -0,0 +1,29 @@
+import os
+
+path = "/usr/tmp"
+
+# Check current working directory.
+retval = os.getcwd()
+print "Current working directory %s" % retval
+
+files = os.listdir(os.getcwd())
+
+for file_ in files:
+    print file_
+    appName = file_[0:-4]
+    webModule = appName[:-3] + 'Web'
+    webModuleFile = webModule + '.war'
+    
+    config = '[ -nopreCompileJSPs -distributeApp -nouseMetaDataFromBinary -nodeployejb -appname ' + appName + ' -createMBeansForResources -noreloadEnabled -deployws -validateinstall warn -processEmbeddedConfig -filepermission .*\.dll=755#.*\.so=755#.*\.a=755#.*\.sl=755 -noallowDispatchRemoteInclude -noallowServiceRemoteInclude -asyncRequestDispatchType DISABLED -nouseAutoLink -noenableClientModule -clientMode isolated -novalidateSchema -MapModulesToServers [[ ' + webModule + ' ' + webModuleFile + ',WEB-INF/web.xml WebSphere:cell=soaprodmon1Cell01,cluster=BPMDEVPS.AppCluster ]]]' 
+
+    try:
+        AdminApp.install(os.getcwd() + "/" + file_, config)
+        print file_ + "has been installed"
+    except:
+        print "Failed installing app: " + appName
+
+print "Installations finished!, Saving configurations"
+AdminConfig.save()
+
+print "Synchronizing active nodes in the cell"
+AdminNodeManagement.syncActiveNodes()
