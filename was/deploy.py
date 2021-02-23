ce612c1496876a0438908b192b24c1a5f8c88e6c
Iresh Dissanayaka <socialexpz1@gmail.com>
2021-02-23 17:33:58 +0530

    Slight improvments
    Convert config string to the list form
    Replace MapModulesToServers options with wildcards to keep the default value

--- was/deploy.py
+++ was/deploy.py
@@ -7,21 +7,53 @@ print "Current working directory %s" % retval
 files = os.listdir(os.getcwd())
 
 for file_ in files:
-    print file_
+    print (file_)
+    filePath = os.getcwd() + "/" + file_
     appName = file_[0:-4]
-    webModule = appName[:-3] + 'Web'
-    webModuleFile = webModule + '.war'
-    
-    config = '[ -nopreCompileJSPs -distributeApp -nouseMetaDataFromBinary -nodeployejb -appname ' + appName + ' -createMBeansForResources -noreloadEnabled -deployws -validateinstall warn -processEmbeddedConfig -filepermission .*\.dll=755#.*\.so=755#.*\.a=755#.*\.sl=755 -noallowDispatchRemoteInclude -noallowServiceRemoteInclude -asyncRequestDispatchType DISABLED -nouseAutoLink -noenableClientModule -clientMode isolated -novalidateSchema -MapModulesToServers [[ ' + webModule + ' ' + webModuleFile + ',WEB-INF/web.xml WebSphere:cell=soaprodmon1Cell01,cluster=BPMDEVPS.AppCluster ]]]' 
+    config = [
+        "-nopreCompileJSPs",
+        "-distributeApp",
+        "-nouseMetaDataFromBinary",
+        "-nodeployejb",
+        "-appname",
+        appName,
+        "-createMBeansForResources",
+        "-noreloadEnabled",
+        "-deployws",
+        "-validateinstall",
+        "warn",
+        "-noprocessEmbeddedConfig",
+        "-filepermission",
+        ".*\.dll=755#.*\.so=755#.*\.a=755#.*\.sl=755",
+        "-noallowDispatchRemoteInclude",
+        "-noallowServiceRemoteInclude",
+        "-asyncRequestDispatchType",
+        "DISABLED",
+        "-nouseAutoLink",
+        "-noenableClientModule",
+        "-clientMode",
+        "isolated",
+        "-novalidateSchema",
+        "-MapModulesToServers",
+        [
+            [
+                ".*",
+                ".*",
+                "WebSphere:cell=soaprodmon1Cell01,cluster=BPMDEVPS.AppCluster",
+            ]
+        ],
+        "-MetadataCompleteForModules",
+        [[".*", ".*", "true"]],
+    ]
 
     try:
-        AdminApp.install(os.getcwd() + "/" + file_, config)
-        print file_ + "has been installed"
+        AdminApp.install(filePath, config)
+        print (file_ + "has been installed")
     except:
-        print "Failed installing app: " + appName
+        print ("Failed installing app: " + appName)
 
-print "Installations finished!, Saving configurations"
+print ("Installations finished!, Saving configurations")
 AdminConfig.save()
 
-print "Synchronizing active nodes in the cell"
+print ("Synchronizing active nodes in the cell")
 AdminNodeManagement.syncActiveNodes()
