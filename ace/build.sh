cee0bcc86a8864afd5f3ad67a84f01da8083af59
Iresh M Dissanayaka <socialexpz1@gmail.com>
2022-03-26 19:55:21 +0530

    refactor variables

--- ace/build.sh
+++ ace/build.sh
@@ -4,9 +4,14 @@ shopt -s globstar
 
 SHARED_LIBS_URL="$1"
 
+# This should be unique and applications/libraries shouldn't use this 
+# as repository directory or application name. A random string
+# is appended
+APPLICATION_DEFAULT_DIR="${APPLICATION_DEFAULT_DIR:-APPLICATION_5JITHW8YDN7KSW}"
+
 resolveDependencies() {
-	DEP="$1"
-	python3 `dirname $0`/resolve_dependencies.py $SHARED_LIBS_URL $DEP
+	DEPENDENCIES="$1"
+	python3 `dirname $0`/resolve_dependencies.py $SHARED_LIBS_URL $DEPENDENCIES
 }
 
 build() {
@@ -15,15 +20,16 @@ build() {
 	mqsicreatebar -data `pwd` -b "${APP_NAME}.bar" -a "$APP_NAME" -deployAsSource
 }
 
-APP=`xmllint --format ./**/.project | grep -m 1 -oP '<name>\K\N+(?=</name>)'`
-APP_PATH=`dirname ./**/.project`
-DEPENDENCIES=`xmllint --format ./**/.project \
+APP_DOT_PROJECT=$APPLICATION_DEFAULT_DIR/**/.project
+APP_NAME=`xmllint --format $APP_DOT_PROJECT | grep -m 1 -oP '<name>\K\N+(?=</name>)'`
+APP_PATH=`dirname $APP_DOT_PROJECT`
+DEPENDENCIES=`xmllint --format $APP_DOT_PROJECT \
 		| grep -oP '<project>\K\N+(?=</project>)' \
 		| awk 'BEGIN {lines=""} {lines = lines " " $1} END {print lines}'`
 
-ln -sf "$APP_PATH" "$APP"
+ln -sf "$APP_PATH" "$APP_NAME"
 
 source `dirname $0`/initenv.sh
 resolveDependencies "$DEPENDENCIES"
-build "$APP" "$DEPENDENCIES"
-echo "$APP" > appname
+build "$APP_NAME" "$DEPENDENCIES"
+echo "$APP_NAME" > appname
