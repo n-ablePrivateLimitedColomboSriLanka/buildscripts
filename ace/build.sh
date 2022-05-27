e5c022d8ecff3dd72c49e78eb488cb510b477b7e
Iresh M Dissanayaka <socialexpz1@gmail.com>
2022-05-27 17:10:06 +0530

    ability to apply a configuration file to the application bar file using mqsiapplybaroverride

--- ace/build.sh
+++ ace/build.sh
@@ -3,6 +3,7 @@
 shopt -s globstar
 
 SHARED_LIBS_URL="$1"
+BRANCH_NAME="$2"
 
 # This should be unique and applications/libraries shouldn't use this 
 # as repository directory or application name. A random string
@@ -20,6 +21,13 @@ build() {
 	mqsicreatebar -data `pwd` -b "${APP_NAME}.bar" -a "$APP_NAME" -deployAsSource
 }
 
+applyConfig() {
+	APP_NAME="$1"
+	CONFIG_FILE="$2"
+	# Apply the overrides to the bar file
+	mqsiapplybaroverride -b "${APP_NAME}.bar" -p "$CONFIG_FILE" -k "$APP_NAME"
+}
+
 APP_DOT_PROJECT=$APPLICATION_DEFAULT_DIR/**/.project
 APP_NAME=`xmllint --format $APP_DOT_PROJECT | grep -m 1 -oP '<name>\K\N+(?=</name>)'`
 APP_PATH=`dirname $APP_DOT_PROJECT`
@@ -32,4 +40,6 @@ ln -sf "$APP_PATH" "$APP_NAME"
 source `dirname $0`/initenv.sh
 resolveDependencies "$DEPENDENCIES"
 build "$APP_NAME" "$DEPENDENCIES"
+CONFIG_FILE="${APP_NAME}/${APP_NAME}.conf.${BRANCH_NAME}"
+applyConfig "$APP_NAME" "$CONFIG_FILE"
 echo "$APP_NAME" > appname
