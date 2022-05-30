2e6a656e53eec18045a7cd32312b1993666b6850
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-05-30 03:22:10 +0000

    make ace installation path a variable

--- ace/initenv.sh
+++ ace/initenv.sh
@@ -1,8 +1,9 @@
 initEnv() {
     # Setup environment for ACE toolkit    
-    . /opt/ace-12.0.3.0/server/bin/mqsiprofile
+    ACE_INSTALLATION=/opt/ibm/ace-12
+    . $ACE_INSTALLATION/server/bin/mqsiprofile
 
-    export CLASSPATH=/opt/ace-12.0.3.0/server/messages:/opt/ace-12.0.3.0/common/classes:/opt/ace-12.0.3.0/server/classes:/opt/ace-12.0.3.0/common/classes/IntegrationAPI.jar:/opt/ace-12.0.3.0/server/classes/brokerutil.jar:/var/mqsi/common/wsrr
+    export CLASSPATH=$ACE_INSTALLATION/server/messages:$ACE_INSTALLATION/common/classes:$ACE_INSTALLATION/server/classes:$ACE_INSTALLATION/common/classes/IntegrationAPI.jar:$ACE_INSTALLATION/server/classes/brokerutil.jar:/var/mqsi/common/wsrr
 
     #Make sure the X server or Xvfb is running    
     if ! ( pgrep -x Xorg > /dev/null || pgrep -x Xvfb > /dev/null ); then
