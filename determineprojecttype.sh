551e9771ca0e9c3a8864a242a54d57558d592465
Iresh M Dissanayaka <socialexpz1@gmail.com>
2023-01-05 15:56:55 +0530

    improve determineprojecttype.sh to identify ACE project type as well

--- determineprojecttype.sh
+++ determineprojecttype.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-[ -f ./pom.xml ] && grep java ./pom.xml > /dev/null && (echo JAVA; exit)
+PROJECT_DIR="$1"
 
-echo NOTJAVA
+[ -f $PROJECT_DIR/pom.xml ] && grep java $PROJECT_DIR/pom.xml > /dev/null && echo JAVA && exit 0
+
+[ -f $PROJECT_DIR/.project ] && grep projectDescription $PROJECT_DIR/.project > /dev/null && echo ACE && exit 0
+
+echo UNDEFINED
